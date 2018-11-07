349f8485382d76417593b178036de2a9f9dbba63
Vedant Jain <vedjain85@gmail.com>
2018-11-07 08:45:52 -0600

    Update randomized_search.py

--- python/spark_sklearn/randomized_search.py
+++ python/spark_sklearn/randomized_search.py
@@ -272,8 +272,160 @@ class RandomizedSearchCV(BaseSearchCV):
             pre_dispatch=pre_dispatch, error_score=error_score,
             return_train_score=return_train_score)
 
-    def _run_search(self, evaluate_candidates):
-        """Search n_iter candidates from param_distributions"""
-        evaluate_candidates(ParameterSampler(
-            self.param_distributions, self.n_iter,
-            random_state=self.random_state))
+        self.sc = sc
+        self.param_grid = param_grid
+
+        self.cv_results_ = None
+        _check_param_grid(param_grid)
+    
+    def fit(self, X, y=None, groups=None):
+        """Run fit with all sets of parameters.
+        
+        Parameters
+        ----------
+        
+        X : array-like, shape = [n_samples, n_features]
+            Training vector, where n_samples is the number of samples and
+            n_features is the number of features.
+        
+        y : array-like, shape = [n_samples] or [n_samples, n_output], optional
+            Target relative to X for classification or regression;
+            None for unsupervised learning.
+        
+        groups : array-like, with shape (n_samples,), optional
+            Group labels for the samples used while splitting the dataset into
+            train/test set.
+        """
+        return self._fit(X, y, groups, ParameterGrid(self.param_grid))
+
+    def _fit(self, X, y, groups, parameter_iterable):
+
+        estimator = self.estimator
+        cv = check_cv(self.cv, y, classifier=is_classifier(estimator))
+
+        self.scorer_ = check_scoring(self.estimator, scoring=self.scoring)
+
+        X, y, groups = indexable(X, y, groups)
+        n_splits = cv.get_n_splits(X, y, groups)
+        
+        if self.verbose > 0 and isinstance(parameter_iterable, Sized):
+            n_candidates = len(parameter_iterable)
+            print("Fitting {0} folds for each of {1} candidates, totalling"
+                  " {2} fits".format(n_splits, n_candidates,
+                                     n_candidates * n_splits))
+
+        base_estimator = clone(self.estimator)
+
+        param_grid = [(parameters, train, test) for parameters in parameter_iterable
+                                                for train, test in list(cv.split(X, y, groups))]
+        # Because the original python code expects a certain order for the elements, we need to
+        # respect it.
+        indexed_param_grid = list(zip(range(len(param_grid)), param_grid))
+        par_param_grid = self.sc.parallelize(indexed_param_grid, len(indexed_param_grid))
+        X_bc = self.sc.broadcast(X)
+        y_bc = self.sc.broadcast(y)
+
+        scorer = self.scorer_
+        verbose = self.verbose
+        error_score = self.error_score
+        fit_params = self.fit_params
+        return_train_score = self.return_train_score
+        fas = _fit_and_score
+
+        def fun(tup):
+            (index, (parameters, train, test)) = tup
+            local_estimator = clone(base_estimator)
+            local_X = X_bc.value
+            local_y = y_bc.value
+            res = fas(local_estimator, local_X, local_y, scorer, train, test, verbose,
+                      parameters, fit_params,
+                      return_train_score=return_train_score,
+                      return_n_test_samples=True, return_times=True,
+                      return_parameters=True, error_score=error_score)
+            return (index, res)
+        indexed_out0 = dict(par_param_grid.map(fun).collect())
+        out = [indexed_out0[idx] for idx in range(len(param_grid))]
+        if return_train_score:
+            (train_scores, test_scores, test_sample_counts, fit_time,
+             score_time, parameters) = zip(*out)
+        else:
+            (test_scores, test_sample_counts, fit_time, score_time, parameters) = zip(*out)
+        X_bc.unpersist()
+        y_bc.unpersist()
+
+        candidate_params = parameters[::n_splits]
+        n_candidates = len(candidate_params)
+
+        results = dict()
+
+        def _store(key_name, array, weights=None, splits=False, rank=False):
+            """A small helper to store the scores/times to the cv_results_"""
+            # When iterated first by splits, then by parameters
+            array = np.array(array, dtype=np.float64).reshape(n_candidates,
+                                                              n_splits)
+            if splits:
+                for split_i in range(n_splits):
+                    results["split%d_%s"
+                            % (split_i, key_name)] = array[:, split_i]
+
+            array_means = np.average(array, axis=1, weights=weights)
+            results['mean_%s' % key_name] = array_means
+            # Weighted std is not directly available in numpy
+            array_stds = np.sqrt(np.average((array -
+                                             array_means[:, np.newaxis]) ** 2,
+                                            axis=1, weights=weights))
+            results['std_%s' % key_name] = array_stds
+
+            if rank:
+                results["rank_%s" % key_name] = np.asarray(
+                    rankdata(-array_means, method='min'), dtype=np.int32)
+
+        # Computed the (weighted) mean and std for test scores alone
+        # NOTE test_sample counts (weights) remain the same for all candidates
+        test_sample_counts = np.array(test_sample_counts[:n_splits],
+                                      dtype=np.int)
+
+        _store('test_score', test_scores, splits=True, rank=True,
+               weights=test_sample_counts if self.iid else None)
+        if self.return_train_score:
+            _store('train_score', train_scores, splits=True)
+        _store('fit_time', fit_time)
+        _store('score_time', score_time)
+
+        best_index = np.flatnonzero(results["rank_test_score"] == 1)[0]
+        best_parameters = candidate_params[best_index]
+
+        # Use one MaskedArray and mask all the places where the param is not
+        # applicable for that candidate. Use defaultdict as each candidate may
+        # not contain all the params
+        param_results = defaultdict(partial(MaskedArray,
+                                            np.empty(n_candidates,),
+                                            mask=True,
+                                            dtype=object))
+        for cand_i, params in enumerate(candidate_params):
+            for name, value in params.items():
+                # An all masked empty array gets created for the key
+                # `"param_%s" % name` at the first occurence of `name`.
+                # Setting the value at an index also unmasks that index
+                param_results["param_%s" % name][cand_i] = value
+
+        results.update(param_results)
+
+        # Store a list of param dicts at the key 'params'
+        results['params'] = candidate_params
+
+        self.cv_results_ = results
+        self.best_index_ = best_index
+        self.n_splits_ = n_splits
+
+        if self.refit:
+            # fit the best estimator using the entire dataset
+            # clone first to work around broken estimators
+            best_estimator = clone(base_estimator).set_params(
+                **best_parameters)
+            if y is not None:
+                best_estimator.fit(X, y, **fit_params)
+            else:
+                best_estimator.fit(X, **fit_params)
+            self.best_estimator_ = best_estimator
+        return self
